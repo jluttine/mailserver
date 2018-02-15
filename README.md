afd7b50912cb34e8d01d059055bb350f1ff0e62e
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2018-02-15 11:31:21 +0200

    Put relay and receiver configuration to JSON files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-This is your initial Salmon application.
+# Personal Salmon mailserver
+
+Relay and receiver configurations are read from `relay_config.json` and
+`receiver_config.json` files from the current working directory. This allows one
+to run tests etc from the root directory of this repository because there are
+some dummy configuration files. However, in production, the working directory
+should be something else, for instance, `/var/lib/salmon` and one should put
+actual configuration files there so that other users don't have access to them.
--- mailserver/config/settings.py
+++ mailserver/config/settings.py
@@ -1,5 +1,7 @@
 # This file contains python variables that configure Salmon for email processing.
 
+import json
+
 # You may add additional parameters such as `username' and `password' if your
 # relay server requires authentication, `starttls' (boolean) or `ssl' (boolean)
 # for secure connections.
@@ -7,10 +9,9 @@
 # Add these configurations to a JSON file settings.json in the directory in
 # which Salmon is run and set permissions properly so the configuration is a)
 # out of version control and b) not accessible to other users of the computer.
-import json
-relay_config = json.load(open("settings.json"))
+relay_config = json.load(open("relay_config.json", "r"))
 
-receiver_config = {'host': '0.0.0.0', 'port': 25}
+receiver_config = json.load(open("receiver_config.json", "r"))
 
 handlers = ['mailserver.app.handlers.sample']
 
